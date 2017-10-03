29fa0729dc1221b78c0e6ed7a5d2909bd494c189
Doublon <tussetquentin07@hotmail.com>
2017-10-03 14:07:06 +0200

    Debug registre utl_file

--- AnalyseCI.sql
+++ AnalyseCI.sql
@@ -4,136 +4,130 @@ DECLARE
     requeteBlock varchar2(10000);
     TYPE Liste_Col IS TABLE OF VARCHAR2(25);
     l_col Liste_Col;
-
+    
     TYPE resultStat IS RECORD
-    (
-		   col varchar2(50),
-		   function varchar2(50),
-		   value varchar2(50)
-    );
-
+	(
+		col varchar2(50),
+		function varchar2(50),
+		value varchar2(50)
+	);
+  
     TYPE resultsStat is table OF resultStat;
 
-    resultats resultsStat;
-    prev varchar2(50);
-
-	PROCEDURE save_file(tab IN resultsStat) is
+	resultats resultsStat;
+    	prev varchar2(50);
+    
+	PROCEDURE save_file(tab IN resultsStat) is 
 	BEGIN
 	FOR indx IN 1 .. tab.COUNT LOOP
-  /* Titre */
-        IF coalesce(prev, '-') != tab(indx).col THEN
-            IF  UPPER(tab(indx).col) LIKE '%ID%' THEN
-                utl_file.put_line(fichierStat, ' ');
-                utl_file.put_line(fichierStat, replace(tab(indx-1).col || '_ID', '_', ' '));
-                utl_file.put_line(fichierStat, RPAD('-', 35, '-'));
-            else
-                utl_file.put_line(fichierStat, ' ');
-                utl_file.put_line(fichierStat, replace(tab(indx).col, '_', ' '));
-                utl_file.put_line(fichierStat, RPAD('-', 35, '-'));
-            end if ;
-	    END IF;
-	    utl_file.put_line(fichierStat,
+    dbms_output.put_line(tab(indx).col);
+     IF coalesce(prev, '-') != tab(indx).col THEN
+        IF  UPPER(tab(indx).col) LIKE 'IDE' THEN
+            utl_file.put_line(fichierStat, ' ');
+            utl_file.put_line(fichierStat, replace(tab(indx-1).col || '-ID :', '_', ' '));
+            utl_file.put_line(fichierStat, RPAD('-', 35, '-'));
+        else 
+            utl_file.put_line(fichierStat, ' ');
+            utl_file.put_line(fichierStat, replace(tab(indx).col || ' : ', '_', ' '));
+            utl_file.put_line(fichierStat, RPAD('-', 35, '-'));
+        end if ;
+	 END IF;
+	  utl_file.put_line(fichierStat,
 	    LPAD(
-            CASE tab(indx).function
-                WHEN 'MIN' THEN 'Minimum'
-                WHEN 'MAX' THEN 'Maximum'
-                WHEN 'AVG' THEN 'Moyenne'
-                WHEN 'STDDEV' THEN 'Ecart-type'
-                WHEN 'MEDIAN' THEN 'Médiane'
-                WHEN 'COUNT' THEN 'Nombre d''élements'
-                WHEN 'Q95' THEN '95ème 100ème quantile'
-                WHEN 'Q9995' THEN '9995ème 10000ème quantile' -- c'etait pas 9500e ?? (cf enonce) en vrai, ca fait tres peu de valeurs pour un si gros echantillon
-                WHEN 'NBNULL' THEN 'Valeurs NULL'
-                WHEN 'UNIQUE' THEN 'Valeur unique(s)'
-                ELSE tab(indx).function
-            END
-            , 23, ' ') || ' : ' ||
-                RPAD(tab(indx).value, 15, ' '));
-            prev := tab(indx).col;
+      CASE tab(indx).function 
+        WHEN 'MIN' THEN 'Minimum'
+        WHEN 'MAX' THEN 'Maximum'
+        WHEN 'AVG' THEN 'Moyenne'
+        WHEN 'STDDEV' THEN 'Ecart-type'
+        WHEN 'MEDIAN' THEN 'Médiane'
+        WHEN 'COUNT' THEN 'Nombre d''élements'
+        WHEN 'Q95' THEN '95ème 100ème quantile'
+        WHEN 'Q9995' THEN '9995ème 10000quantile'
+        WHEN 'NBNULL' THEN 'Valeurs NULL'
+        WHEN 'UNIQUE' THEN 'Valeur unique(s)'
+        ELSE tab(indx).function
+      END
+      , 23, ' ') || ' : ' ||
+	    RPAD(tab(indx).value, 15, ' '));
+	  prev := tab(indx).col;
 	END LOOP;
-    -- Ceci est un test, ne pas taper
-    EXCEPTION
-        WHEN OTHERS THEN
-            Procedure_Logs(CURRENT_TIMESTAMP, DBMS_UTILITY.format_error_backtrace, DBMS_UTILITY.format_error_stack, 2);
+
 	END save_file;
-
-    Procedure_Logs(CURRENT_TIMESTAMP, 'Apres requête sur les titres', 'AnalyseCI', 1);
-
+    
     BEGIN
         fichierStat := utl_file.fopen ('MYDIR', 'AnalyseCI.txt', 'W');
         utl_file.put_line (fichierStat, 'Statistiques colonnes');
         utl_file.put_line (fichierStat, RPAD('-', 35, '-'));
-
-    SELECT 'SELECT substr(aliases, 1, instr(aliases, ''_'') -1),substr(aliases, instr(aliases, ''_'')+1, length(aliases) - instr(aliases, ''_'')), value from(SELECT ' ||
+             
+    SELECT 'SELECT substr(aliases, 1, instr(aliases, ''_'') -1),substr(aliases, instr(aliases, ''_'')+1, length(aliases) - instr(aliases, ''_'')), value from(SELECT ' || 
     listagg(
         CASE dt
         WHEN 'VARCHAR2' THEN
-            CASE
+            CASE 
                 WHEN col = 'CERTIFICATION' OR col = 'STATUS' THEN 'COUNT(DISTINCT '|| col ||') '|| col ||'_unique,'
             END ||
-            'MIN(LENGTH('|| col ||')) '|| col ||'_min,' ||
-            'MAX(LENGTH('|| col ||')) '|| col ||'_max,' ||
-            'ROUND(AVG(LENGTH('|| col ||')),2) '|| col ||'_moy,' ||
-            'ROUND(STDDEV(LENGTH('|| col ||')),2) '|| col ||'_ecarttype,' ||
-            'ROUND(MEDIAN(LENGTH('|| col ||')),2) '|| col ||'_medianne,' ||
-            'COUNT(LENGTH('|| col ||')) '|| col ||'_count,' ||
-            'PERCENTILE_CONT(0.95) WITHIN GROUP(ORDER BY LENGTH('|| col ||')) '|| col ||'_q95,' ||
-            'PERCENTILE_CONT(0.9995) WITHIN GROUP(ORDER BY LENGTH('|| col ||')) '|| col ||'_q9995,' ||
+            'MIN(LENGTH('|| col ||')) '|| col ||'_min,' || 
+            'MAX(LENGTH('|| col ||')) '|| col ||'_max,' || 
+            'ROUND(AVG(LENGTH('|| col ||')),2) '|| col ||'_moy,' || 
+            'ROUND(STDDEV(LENGTH('|| col ||')),2) '|| col ||'_ecarttype,' ||  
+            'ROUND(MEDIAN(LENGTH('|| col ||')),2) '|| col ||'_medianne,' ||  
+            'COUNT(LENGTH('|| col ||')) '|| col ||'_count,' ||  
+            'PERCENTILE_CONT(0.95) WITHIN GROUP(ORDER BY LENGTH('|| col ||')) '|| col ||'_q95,' || 
+            'PERCENTILE_CONT(0.9995) WITHIN GROUP(ORDER BY LENGTH('|| col ||')) '|| col ||'_q9995,' || 
             'COUNT(NVL2(LENGTH('|| col ||'), NULL, 1)) '|| col ||'_nbnull'
-
-        WHEN 'NUMBER' THEN
-            'MIN('|| col ||') '|| col ||'_min,' ||
-            'MAX('|| col ||') '|| col ||'_max,' ||
-            'ROUND(AVG('|| col ||'), 2) '|| col ||'_moy,' ||
-            'ROUND(STDDEV('|| col ||'), 2) '|| col ||'_ecarttype,' ||
-            'ROUND(MEDIAN('|| col ||'), 2) '|| col ||'_medianne,' ||
-            'COUNT('|| col ||') '|| col ||'_count,' ||
-            'PERCENTILE_CONT(0.95) WITHIN GROUP(ORDER BY '|| col ||') '|| col ||'_q95,' ||
-            'PERCENTILE_CONT(0.9995) WITHIN GROUP(ORDER BY '|| col ||') '|| col ||'_q9995,' ||
+    
+        WHEN 'NUMBER' THEN 
+            'MIN('|| col ||') '|| col ||'_min,' || 
+            'MAX('|| col ||') '|| col ||'_max,' || 
+            'ROUND(AVG('|| col ||'), 2) '|| col ||'_moy,' || 
+            'ROUND(STDDEV('|| col ||'), 2) '|| col ||'_ecarttype,' ||  
+            'ROUND(MEDIAN('|| col ||'), 2) '|| col ||'_medianne,' ||  
+            'COUNT('|| col ||') '|| col ||'_count,' || 
+            'PERCENTILE_CONT(0.95) WITHIN GROUP(ORDER BY '|| col ||') '|| col ||'_q95,' || 
+            'PERCENTILE_CONT(0.9995) WITHIN GROUP(ORDER BY '|| col ||') '|| col ||'_q9995,' || 
             'COUNT(NVL2('|| col ||', NULL, 1)) '|| col ||'_nbnull'
       END, ', ')
-    WITHIN group (order by col) ||
+    WITHIN group (order by col) || 
     ' FROM movies_ext)' ||
     ' UNPIVOT (value for aliases in('|| listagg(CASE WHEN col = 'CERTIFICATION' OR col = 'STATUS' THEN col ||'_unique, 'END || col || '_min, ' || col || '_max, ' ||col || '_moy, ' ||col || '_ecarttype, ' ||col || '_medianne, ' ||col || '_count, ' ||col || '_q95, ' ||col || '_q9995, ' ||col || '_nbnull', ', ') within group(order by col) ||'))'
     query
     into requeteBlock
     from (select column_name col, data_type dt
           from user_tab_columns
-          where table_name = 'MOVIES_EXT'
+          where table_name = 'MOVIES_EXT'  
           and column_name in ('ID', 'TITLE', 'STATUS','VOTE_AVERAGE','VOTE_COUNT','RUNTIME','CERTIFICATION'));
-
+          
     Execute IMMEDIATE requeteBlock BULK COLLECT INTO resultats;
     save_file(resultats);
-    Procedure_Logs(CURRENT_TIMESTAMP, 'Apres requête des colonnes simples', 'AnalyseCI', 1);
-
+    
     l_col:=Liste_Col('GENRES','DIRECTORS');
 	FOR indx IN 1 .. l_col.count LOOP
 		requeteBlock := '
-		SELECT substr(aliases, 1, instr(aliases, ''_'') -1),substr(aliases, instr(aliases, ''_'')+1, length(aliases) - instr(aliases, ''_'')),
+		SELECT substr(aliases, 1, instr(aliases, ''_'') -1),substr(aliases, instr(aliases, ''_'')+1, length(aliases) - instr(aliases, ''_'')), 
 		value
 	    FROM
 	    (
-	      SELECT
-	      MIN(id) id_'|| l_col(indx) ||'_min,
-          MAX(id) id_'|| l_col(indx) ||'_max,
-          ROUND(AVG(id),2) id_'|| l_col(indx) ||'_moy,
-          ROUND(STDDEV(id),2) id_'|| l_col(indx) ||'_ecarttype,
-          ROUND(MEDIAN(id),2) id_'|| l_col(indx) ||'_medianne,
-          COUNT(id) id_'|| l_col(indx) ||'_count,
-          PERCENTILE_CONT(0.95) WITHIN GROUP(ORDER BY id) id_'|| l_col(indx) ||'_q95,
-          PERCENTILE_CONT(0.9995) WITHIN GROUP(ORDER BY id) id_'|| l_col(indx) ||'_q9995,
-          COUNT(NVL2(id, NULL, 1)) id_'|| l_col(indx) ||'_nbnull,
-          min(LENGTH(nom)) '|| l_col(indx) ||'_min,
-          max(LENGTH(nom)) '|| l_col(indx) ||'_max,
-          ROUND(avg(LENGTH(nom)),2) '|| l_col(indx) ||'_moy,
-          ROUND(stddev(LENGTH(nom)),2) '|| l_col(indx) ||'_ecarttype,
-          ROUND(median(LENGTH(nom)),2) '|| l_col(indx) ||'_medianne,
-          count(LENGTH(nom)) '|| l_col(indx) ||'_count,
-          PERCENTILE_CONT(0.95) WITHIN GROUP(ORDER BY LENGTH(nom)) '|| l_col(indx) ||'_q95,
-          PERCENTILE_CONT(0.9995) WITHIN GROUP(ORDER BY LENGTH(nom)) '|| l_col(indx) ||'_q9995,
+	      SELECT 
+	      MIN(ide) ide_'|| l_col(indx) ||'_min,
+          MAX(ide) ide_'|| l_col(indx) ||'_max,
+          ROUND(AVG(ide),2) ide_'|| l_col(indx) ||'_moy, 
+          ROUND(STDDEV(ide),2) ide_'|| l_col(indx) ||'_ecarttype,  
+          ROUND(MEDIAN(ide),2) ide_'|| l_col(indx) ||'_medianne,  
+          COUNT(ide) ide_'|| l_col(indx) ||'_count,  
+          PERCENTILE_CONT(0.95) WITHIN GROUP(ORDER BY ide) ide_'|| l_col(indx) ||'_q95,
+          PERCENTILE_CONT(0.9995) WITHIN GROUP(ORDER BY ide) ide_'|| l_col(indx) ||'_q9995, 
+          COUNT(NVL2(ide, NULL, 1)) ide_'|| l_col(indx) ||'_nbnull,
+          min(LENGTH(nom)) '|| l_col(indx) ||'_min, 
+          max(LENGTH(nom)) '|| l_col(indx) ||'_max, 
+          ROUND(avg(LENGTH(nom)),2) '|| l_col(indx) ||'_moy, 
+          ROUND(stddev(LENGTH(nom)),2) '|| l_col(indx) ||'_ecarttype, 
+          ROUND(median(LENGTH(nom)),2) '|| l_col(indx) ||'_medianne, 
+          count(LENGTH(nom)) '|| l_col(indx) ||'_count, 
+          PERCENTILE_CONT(0.95) WITHIN GROUP(ORDER BY LENGTH(nom)) '|| l_col(indx) ||'_q95, 
+          PERCENTILE_CONT(0.9995) WITHIN GROUP(ORDER BY LENGTH(nom)) '|| l_col(indx) ||'_q9995, 
           COUNT(NVL2(LENGTH(nom), NULL, 1)) '|| l_col(indx) ||'_nbnull
           FROM (
-             with split(champs, debut, fin) as
+             with split(champs, debut, fin) as 
             (
               Select '|| l_col(indx) ||' , 1 debut, regexp_instr('||l_col(indx)||',''‖'') fin from movies_ext
               union all
@@ -144,19 +138,19 @@ DECLARE
                 select *
                 from(
                     select distinct
-                    cast(REGEXP_SUBSTR(champs,''[^․]+'') as number) id,
+                    cast(REGEXP_SUBSTR(champs,''[^․]+'') as number) ide,
                     SUBSTR(REGEXP_SUBSTR(champs,''[^‖]+''),cast((regexp_instr(champs,''․'')+1)as number))as nom
                     from split)
-                    where id is not null
+                    where ide is not null 
                     and nom is not null
                     )
 		)
 		UNPIVOT(value for aliases IN(
-        '|| l_col(indx) ||'_min, '|| l_col(indx) ||'_max, '|| l_col(indx) ||'_moy, '|| l_col(indx) ||'_ecarttype, '|| l_col(indx) ||'_medianne, '|| l_col(indx) ||'_count, '|| l_col(indx) ||'_q95, '|| l_col(indx) ||'_q9995, '|| l_col(indx) ||'_nbnull, '||
-        'id_'||l_col(indx) ||'_min, '|| 'id_'||l_col(indx) ||'_max, '||
-        'id_'||l_col(indx) ||'_moy, '|| 'id_'||l_col(indx) ||'_ecarttype, '|| 'id_'||l_col(indx) ||'_medianne, '||
-        'id_'||l_col(indx) ||'_count, '|| 'id_'||l_col(indx) ||'_q95, '|| 'id_'||l_col(indx) ||'_q9995, '||
-        'id_'||l_col(indx) ||'_nbnull))';
+        '|| l_col(indx) ||'_min, '|| l_col(indx) ||'_max, '|| l_col(indx) ||'_moy, '|| l_col(indx) ||'_ecarttype, '|| l_col(indx) ||'_medianne, '|| l_col(indx) ||'_count, '|| l_col(indx) ||'_q95, '|| l_col(indx) ||'_q9995, '|| l_col(indx) ||'_nbnull, '|| 
+        'ide_'||l_col(indx) ||'_min, '|| 'ide_'||l_col(indx) ||'_max, '|| 
+        'ide_'||l_col(indx) ||'_moy, '|| 'ide_'||l_col(indx) ||'_ecarttype, '|| 'ide_'||l_col(indx) ||'_medianne, '|| 
+        'ide_'||l_col(indx) ||'_count, '|| 'ide_'||l_col(indx) ||'_q95, '|| 'ide_'||l_col(indx) ||'_q9995, '|| 
+        'ide_'||l_col(indx) ||'_nbnull))';
 	    EXECUTE IMMEDIATE requeteBlock BULK COLLECT INTO resultats;
 	    save_file(resultats);
         /*FOR i IN 1 .. resultats.count
@@ -166,6 +160,4 @@ DECLARE
 	END LOOP;
     utl_file.fclose(fichierStat);
     END ;
-
-    Procedure_Logs(CURRENT_TIMESTAMP, 'Apres requête sur les genres et les directors', 'AnalyseCI', 1);
 END ;
