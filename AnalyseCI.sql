d5859b06cc89d18262ce28c80a496f86a7c6e1e8
Doublon <tussetquentin07@hotmail.com>
2017-10-05 12:22:56 +0200

    Ajout analye acteur dans le tout
    Il ne manque plus que les dates + les excetpions

--- AnalyseCI.sql
+++ AnalyseCI.sql
@@ -42,7 +42,7 @@ DECLARE
         WHEN 'MEDIAN' THEN 'Médiane'
         WHEN 'COUNT' THEN 'Nombre d''élements'
         WHEN 'Q95' THEN '95ème 100ème quantile'
-        WHEN 'Q9995' THEN '9995ème 10000quantile'
+        WHEN 'Q995' THEN '995ème 1000quantile'
         WHEN 'NBNULL' THEN 'Valeurs NULL'
         WHEN 'UNIQUE' THEN 'Valeur unique(s)'
         ELSE tab(indx).function
@@ -73,7 +73,7 @@ DECLARE
             'ROUND(MEDIAN(LENGTH('|| col ||')),2) '|| col ||'_medianne,' ||  
             'COUNT(LENGTH('|| col ||')) '|| col ||'_count,' ||  
             'PERCENTILE_CONT(0.95) WITHIN GROUP(ORDER BY LENGTH('|| col ||')) '|| col ||'_q95,' || 
-            'PERCENTILE_CONT(0.9995) WITHIN GROUP(ORDER BY LENGTH('|| col ||')) '|| col ||'_q9995,' || 
+            'PERCENTILE_CONT(0.995) WITHIN GROUP(ORDER BY LENGTH('|| col ||')) '|| col ||'_q995,' || 
             'COUNT(NVL2(LENGTH('|| col ||'), NULL, 1)) '|| col ||'_nbnull'
     
         WHEN 'NUMBER' THEN 
@@ -84,12 +84,12 @@ DECLARE
             'ROUND(MEDIAN('|| col ||'), 2) '|| col ||'_medianne,' ||  
             'COUNT('|| col ||') '|| col ||'_count,' || 
             'PERCENTILE_CONT(0.95) WITHIN GROUP(ORDER BY '|| col ||') '|| col ||'_q95,' || 
-            'PERCENTILE_CONT(0.9995) WITHIN GROUP(ORDER BY '|| col ||') '|| col ||'_q9995,' || 
+            'PERCENTILE_CONT(0.995) WITHIN GROUP(ORDER BY '|| col ||') '|| col ||'_q995,' || 
             'COUNT(NVL2('|| col ||', NULL, 1)) '|| col ||'_nbnull'
       END, ', ')
     WITHIN group (order by col) || 
     ' FROM movies_ext)' ||
-    ' UNPIVOT (value for aliases in('|| listagg(CASE WHEN col = 'CERTIFICATION' OR col = 'STATUS' THEN col ||'_unique, 'END || col || '_min, ' || col || '_max, ' ||col || '_moy, ' ||col || '_ecarttype, ' ||col || '_medianne, ' ||col || '_count, ' ||col || '_q95, ' ||col || '_q9995, ' ||col || '_nbnull', ', ') within group(order by col) ||'))'
+    ' UNPIVOT (value for aliases in('|| listagg(CASE WHEN col = 'CERTIFICATION' OR col = 'STATUS' THEN col ||'_unique, 'END || col || '_min, ' || col || '_max, ' ||col || '_moy, ' ||col || '_ecarttype, ' ||col || '_medianne, ' ||col || '_count, ' ||col || '_q95, ' ||col || '_q995, ' ||col || '_nbnull', ', ') within group(order by col) ||'))'
     query
     into requeteBlock
     from (select column_name col, data_type dt
@@ -115,7 +115,7 @@ DECLARE
           ROUND(MEDIAN(ide),2) ide_'|| l_col(indx) ||'_medianne,  
           COUNT(ide) ide_'|| l_col(indx) ||'_count,  
           PERCENTILE_CONT(0.95) WITHIN GROUP(ORDER BY ide) ide_'|| l_col(indx) ||'_q95,
-          PERCENTILE_CONT(0.9995) WITHIN GROUP(ORDER BY ide) ide_'|| l_col(indx) ||'_q9995, 
+          PERCENTILE_CONT(0.995) WITHIN GROUP(ORDER BY ide) ide_'|| l_col(indx) ||'_q995, 
           COUNT(NVL2(ide, NULL, 1)) ide_'|| l_col(indx) ||'_nbnull,
           min(LENGTH(nom)) '|| l_col(indx) ||'_min, 
           max(LENGTH(nom)) '|| l_col(indx) ||'_max, 
@@ -124,14 +124,14 @@ DECLARE
           ROUND(median(LENGTH(nom)),2) '|| l_col(indx) ||'_medianne, 
           count(LENGTH(nom)) '|| l_col(indx) ||'_count, 
           PERCENTILE_CONT(0.95) WITHIN GROUP(ORDER BY LENGTH(nom)) '|| l_col(indx) ||'_q95, 
-          PERCENTILE_CONT(0.9995) WITHIN GROUP(ORDER BY LENGTH(nom)) '|| l_col(indx) ||'_q9995, 
+          PERCENTILE_CONT(0.995) WITHIN GROUP(ORDER BY LENGTH(nom)) '|| l_col(indx) ||'_q995, 
           COUNT(NVL2(LENGTH(nom), NULL, 1)) '|| l_col(indx) ||'_nbnull
           FROM (
              with split(champs, debut, fin) as 
             (
-              Select '|| l_col(indx) ||' , 1 debut, instr('||l_col(indx)||',''‖'') fin from movies_ext
+              Select '|| l_col(indx) ||' , 1 debut, regexp_instr('||l_col(indx)||',''‖'') fin from movies_ext
               union all
-              select champs, fin + 1, instr(champs, ''‖'', fin+1)
+              select champs, fin + 1, regexp_instr(champs, ''‖'', fin+1)
               from split
               where fin <> 0
             )
@@ -139,25 +139,77 @@ DECLARE
                 from(
                     select distinct
                     cast(REGEXP_SUBSTR(champs,''[^․]+'') as number) ide,
-                    SUBSTR(REGEXP_SUBSTR(champs,''[^‖]+''),cast((instr(champs,''․'')+1)as number))as nom
+                    SUBSTR(REGEXP_SUBSTR(champs,''[^‖]+''),cast((regexp_instr(champs,''․'')+1)as number))as nom
                     from split)
                     where ide is not null 
                     and nom is not null
                     )
 		)
 		UNPIVOT(value for aliases IN(
-        '|| l_col(indx) ||'_min, '|| l_col(indx) ||'_max, '|| l_col(indx) ||'_moy, '|| l_col(indx) ||'_ecarttype, '|| l_col(indx) ||'_medianne, '|| l_col(indx) ||'_count, '|| l_col(indx) ||'_q95, '|| l_col(indx) ||'_q9995, '|| l_col(indx) ||'_nbnull, '|| 
+        '|| l_col(indx) ||'_min, '|| l_col(indx) ||'_max, '|| l_col(indx) ||'_moy, '|| l_col(indx) ||'_ecarttype, '|| l_col(indx) ||'_medianne, '|| l_col(indx) ||'_count, '|| l_col(indx) ||'_q95, '|| l_col(indx) ||'_q995, '|| l_col(indx) ||'_nbnull, '|| 
         'ide_'||l_col(indx) ||'_min, '|| 'ide_'||l_col(indx) ||'_max, '|| 
         'ide_'||l_col(indx) ||'_moy, '|| 'ide_'||l_col(indx) ||'_ecarttype, '|| 'ide_'||l_col(indx) ||'_medianne, '|| 
-        'ide_'||l_col(indx) ||'_count, '|| 'ide_'||l_col(indx) ||'_q95, '|| 'ide_'||l_col(indx) ||'_q9995, '|| 
+        'ide_'||l_col(indx) ||'_count, '|| 'ide_'||l_col(indx) ||'_q95, '|| 'ide_'||l_col(indx) ||'_q995, '|| 
         'ide_'||l_col(indx) ||'_nbnull))';
 	    EXECUTE IMMEDIATE requeteBlock BULK COLLECT INTO resultats;
 	    save_file(resultats);
-        /*FOR i IN 1 .. resultats.count
-        LOOP
-            dbms_output.put_line(resultats(i).col || ' ' || resultats(i).function || ' ' || resultats(i).value);
-        end loop;*/
-	END LOOP;
+		END LOOP;
+		
+		SELECT substr(aliases, 1, instr(aliases, '_') -1),substr(aliases, instr(aliases, '_')+1, length(aliases) - instr(aliases, '_')), value
+        BULK COLLECT INTO resultats
+	    FROM
+	    (
+	      SELECT 
+          min(LENGTH(actors)) actors_min, 
+          max(LENGTH(actors)) actors_max, 
+          ROUND(avg(LENGTH(actors)),2) actors_moy, 
+          ROUND(stddev(LENGTH(actors)),2) actors_ecarttype, 
+          ROUND(median(LENGTH(actors)),2) actors_medianne, 
+          count(LENGTH(actors)) actors_count, 
+          PERCENTILE_CONT(0.95) WITHIN GROUP(ORDER BY LENGTH(actors)) actors_q95, 
+          PERCENTILE_CONT(0.995) WITHIN GROUP(ORDER BY LENGTH(actors)) actors_q995,
+          MIN(ide) ide_actors_min,
+          MAX(ide) ide_actors_max,
+          ROUND(AVG(ide),2) ide_actors_moy, 
+          ROUND(STDDEV(ide),2) ide_actors_ecarttype,  
+          ROUND(MEDIAN(ide),2) ide_actors_medianne,  
+          COUNT(ide) ide_actors_count,  
+          PERCENTILE_CONT(0.95) WITHIN GROUP(ORDER BY ide) ide_actors_q95,
+          PERCENTILE_CONT(0.995) WITHIN GROUP(ORDER BY ide) ide_actors_q995, 
+          min(LENGTH(roles)) roles_min, 
+          max(LENGTH(roles)) roles_max, 
+          ROUND(avg(LENGTH(roles)),2) roles_moy, 
+          ROUND(stddev(LENGTH(roles)),2) roles_ecarttype, 
+          ROUND(median(LENGTH(roles)),2) roles_medianne, 
+          count(LENGTH(roles)) roles_count, 
+          PERCENTILE_CONT(0.95) WITHIN GROUP(ORDER BY LENGTH(roles)) roles_q95, 
+          PERCENTILE_CONT(0.995) WITHIN GROUP(ORDER BY LENGTH(roles)) roles_q995,
+		  COUNT(NVL2(roles, NULL, 1)) roles_nbnull		  
+          FROM (
+				with split(champs, debut, fin) as 
+				(
+					Select actors , 1 debut, instr(actors,'‖') fin 
+					from movies_ext
+					union all
+					select champs, fin + 1, instr(champs, '‖', fin+1)
+					from split
+					where fin <> 0
+				)
+				select distinct
+				cast(substr(tuple, 1, instr(tuple, '․') -1) as number) ide,
+				SUBSTR(tuple, instr(tuple, '․')+1, instr(tuple, '․', 1, 2) - instr(tuple, '․')-1) actors,
+				substr(tuple, instr(tuple, '․', 1, 2)+1, LENGTH(tuple) - instr(tuple, '․', 1, 2)) roles
+				from(
+					SELECT SUBSTR(champs, debut, coalesce(fin, length(champs)+1) - debut) tuple
+					FROM split -- 1.593.318
+					WHERE champs IS NOT NULL --1.456.115
+					)
+                )
+		)
+	UNPIVOT (value for aliases IN (actors_min,actors_max,actors_ecarttype,actors_medianne,actors_count,actors_q95,actors_q995,
+                                    ide_actors_min,ide_actors_max,ide_actors_moy,ide_actors_ecarttype,ide_actors_medianne,ide_actors_count,ide_actors_q95,ide_actors_q995,
+									roles_min,roles_max,roles_moy,roles_ecarttype,roles_medianne,roles_count,roles_q95,roles_q995,roles_actors_nbnull));
+	save_file(resultats);
     utl_file.fclose(fichierStat);
     END ;
 END ;
