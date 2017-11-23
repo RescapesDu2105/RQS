e7326fff99c5c10d6111c3b5b5194e1cc3560651
Doublon <tussetquentin07@hotmail.com>
2017-11-23 13:13:55 +0100

    Maj VerifActeur
    Comme la prof voulait

--- BP/BPE/VerifActeur_v1/Procedure_VerifActeur.sql
+++ BP/BPE/VerifActeur_v1/Procedure_VerifActeur.sql
@@ -1,4 +1,4 @@
-create or replace PROCEDURE VerifActeur(IdAct IN Artists.idArt%type)
+create or replace PROCEDURE VerifActeur(Id_Film IN Jouer.Film%type)
 IS
     TYPE Fiche_Acteur IS RECORD(
         id NUMBER(7,0),
@@ -15,7 +15,6 @@ IS
         Annee_Sortie NUMBER(4));
     Biography Biography_Rec;
 
-   -- x   UTL_HTTP.HTML_PIECES;
     req UTL_HTTP.REQ;
     res UTL_HTTP.RESP;
     reponse VARCHAR(4000);
@@ -31,97 +30,79 @@ IS
     vLieuNaiss      PEOPLE_EXT.PLACE_OF_BIRTH%TYPE;
     vProfil         PEOPLE_EXT.PROFILE_PATH%TYPE;
 
-    vIdFilm         Films.IdFilm%TYPE;
-    vTitreFilm      Films.Titre%TYPE;
-    vAnneeSortie    NUMBER(4);
-    vRole           Jouer.Role%TYPE;
+    TYPE FilmRec IS RECORD (
+        vIdFilm         Films.IdFilm%TYPE,
+        vTitreFilm      Films.Titre%TYPE,
+        vAnneeSortie    NUMBER(4),
+        vRole           Jouer.Role%TYPE);
+    vFilm FilmRec;
+    
+    TYPE Liste_Acteurs_Id IS TABLE OF Jouer.Artist%TYPE INDEX BY BINARY_INTEGER;
+    l_Act_Id Liste_Acteurs_Id;
 BEGIN
-
-   /* 
-    APEX_JSON.initialize_clob_output;
-   
-   OPEN l_cursor FOR
-    select  
+    SELECT Artist BULK COLLECT INTO l_Act_Id
+    FROM JOUER
+    WHERE Film=Id_Film;
+    
+    FOR indx IN l_Act_Id.FIRST..l_Act_Id.LAST LOOP
+        APEX_JSON.initialize_clob_output;
+        APEX_JSON.open_object;
+    
+        SELECT  
             J.ARTIST as Id,
             Peop.Name as Nom, 
             TO_CHAR(Peop.BIRTHDAY, 'YYYY-MM-DD') as DateNaiss, 
             TO_CHAR(Peop.DEATHDAY, 'YYYY-MM-DD') as DateDeces,
             Peop.PLACE_OF_BIRTH as LieuNaiss,
-            Peop.PROFILE_PATH as Profil,
-            cursor(
-                SELECT 
-                    F2.IdFilm AS Id, 
-                    F2.Titre as Titre,
-                    TO_CHAR(F2.DATE_REAL, 'YYYY') as AnneeSortie,
-                    J2.role as Role 
-                    FROM FILMS F2, Jouer J2 , PEOPLE_EXT Peop2
-                    WHERE J2.Film = F2.idFilm
-                    AND Peop2.id=J2.ARTIST
-                    AND Peop2.id=IdAct) AS Films            
-    from PEOPLE_EXT Peop JOIN Jouer J
-    ON J.ARTIST = Peop.Id
-    WHERE id=IdAct;
+            Peop.PROFILE_PATH as Profil
+            INTO vIdAct, vNomAct, vDateNaiss, vDateDeces, vLieuNaiss, vProfil
+        from PEOPLE_EXT Peop JOIN Jouer J
+        ON J.ARTIST = Peop.Id
+        WHERE id=l_Act_Id(indx)
+        AND film=Id_Film;
     
-    
-    APEX_JSON.open_object;
-    APEX_JSON.write(l_cursor);
-    APEX_JSON.close_object;
-    */
-    
-
-    APEX_JSON.initialize_clob_output;
-    APEX_JSON.open_object;
-
-    SELECT  
-        J.ARTIST as Id,
-        Peop.Name as Nom, 
-        TO_CHAR(Peop.BIRTHDAY, 'YYYY-MM-DD') as DateNaiss, 
-        TO_CHAR(Peop.DEATHDAY, 'YYYY-MM-DD') as DateDeces,
-        Peop.PLACE_OF_BIRTH as LieuNaiss,
-        Peop.PROFILE_PATH as Profil
-        INTO vIdAct, vNomAct, vDateNaiss, vDateDeces, vLieuNaiss, vProfil
-    from PEOPLE_EXT Peop JOIN Jouer J
-    ON J.ARTIST = Peop.Id
-    WHERE id=IdAct;
-
-    APEX_JSON.write('_id', vIdAct);
-    APEX_JSON.write_raw('name', '"' || vNomAct || '"');
-    APEX_JSON.write('birthday', vDateNaiss);
-    APEX_JSON.write_raw('place_of_birth', '"' || vLieuNaiss  || '"'); 
-    
-    SELECT 
-        F2.IdFilm AS Id, 
-        F2.Titre as Titre,
-        EXTRACT(YEAR FROM F2.DATE_REAL) as AnneeSortie,
-        J2.role as Role 
-    INTO vIdFilm, vTitreFilm, vAnneeSortie, vRole
-    FROM FILMS F2, Jouer J2 , PEOPLE_EXT Peop2
-    WHERE J2.Film = F2.idFilm
-    AND Peop2.id=J2.ARTIST
-    AND Peop2.id=IdAct;
-
-    APEX_JSON.open_array('films');    
-    APEX_JSON.open_object;
-    APEX_JSON.write('_id', vIdFilm);
-    APEX_JSON.write_raw('titre', '"' || vTitreFilm || '"');
-    APEX_JSON.write('release_date', vAnneeSortie);
-    APEX_JSON.write_raw('character', '"' || vRole || '"'); 
-    APEX_JSON.close_object;
-    APEX_JSON.close_array;
-    
-    APEX_JSON.close_object;   
-    
-    DBMS_OUTPUT.put_line(APEX_JSON.get_clob_output);
-    
-    actJSON := APEX_JSON.get_clob_output;
-    APEX_JSON.free_output;
-      
-    req := utl_http.begin_request('http://10.0.2.2:8084/VerifActeur_v1', 'POST',' HTTP/1.1');
-    utl_http.set_header(req, 'content-type', 'application/json');
-    utl_http.set_header(req, 'Content-Length', length(actJSON));
-    utl_http.write_text(req, actJSON);
-    res := utl_http.get_response(req);
-    UTL_HTTP.END_RESPONSE(res);
+        APEX_JSON.write('_id', vIdAct);
+        APEX_JSON.write_raw('name', '"' || vNomAct || '"');
+        APEX_JSON.write('birthday', vDateNaiss);
+        APEX_JSON.write_raw('place_of_birth', '"' || vLieuNaiss  || '"'); 
+        
+        SELECT     
+            tb.IdFilm AS Id, 
+            tb.Titre as Titre,
+            EXTRACT(YEAR FROM tb.DATE_REAL) as AnneeSortie,
+            tb.role as Role
+            INTO vFilm
+        FROM(
+            SELECT *
+            FROM Jouer JOIN FILMS
+            ON Jouer.film=films.idfilm
+            WHERE Jouer.ARTIST=l_Act_Id(indx)) tb
+        WHERE tb.film=Id_Film;
+        
+        APEX_JSON.open_array('films');    
+        APEX_JSON.open_object;
+        APEX_JSON.write('_id', vFilm.vIdFilm);
+        APEX_JSON.write_raw('titre', '"' || vFilm.vTitreFilm || '"');
+        APEX_JSON.write('release_date', vFilm.vAnneeSortie);
+        APEX_JSON.write_raw('character', '"' || vFilm.vRole || '"'); 
+        APEX_JSON.close_object;
+        APEX_JSON.close_array;
+        
+        APEX_JSON.close_object;   
+        
+        DBMS_OUTPUT.put_line(APEX_JSON.get_clob_output);
+        
+        actJSON := APEX_JSON.get_clob_output;
+        APEX_JSON.free_output;
+          
+        req := utl_http.begin_request('http://10.0.2.2:8084/VerifActeur_v1', 'POST',' HTTP/1.1');
+        utl_http.set_header(req, 'content-type', 'application/json');
+        utl_http.set_header(req, 'Content-Length', length(actJSON));
+        utl_http.write_text(req, actJSON);
+        res := utl_http.get_response(req);
+        UTL_HTTP.END_RESPONSE(res);
+        
+    END LOOP;
     
     /*SELECT ID , Name, TO_CHAR(BIRTHDAY, 'YYYY-MM-DD'), TO_CHAR(DEATHDAY, 'YYYY-MM-DD'),PLACE_OF_BIRTH,PROFILE_PATH INTO f_Acteur
     FROM PEOPLE_EXT
