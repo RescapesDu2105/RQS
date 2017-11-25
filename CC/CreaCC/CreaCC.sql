0e2e489208fc41b6af15ba8cbfdafa8b20907910
Doublon <tussetquentin07@hotmail.com>
2017-11-25 22:34:09 +0100

    CreaCC

--- CC/CreaCC/CreaCC.sql
+++ CC/CreaCC/CreaCC.sql
@@ -1,7 +1,25 @@
 BEGIN  
     DBMS_XMLSCHEMA.REGISTERSCHEMA(
-        SCHEMAURL => 'http://cc/artiste.xsd',
-        SCHEMADOC => BFILENAME('DIRXML', 'artiste.xsd'),
+        SCHEMAURL => 'http://cc/artistes.xsd',
+        SCHEMADOC => BFILENAME('DIRXML', 'artistes.xsd'),
+        LOCAL => TRUE, 
+		GENTYPES => TRUE, 
+		GENTABLES => FALSE,
+        CSID => NLS_CHARSET_ID('AL32UTF8')
+    );
+	
+	DBMS_XMLSCHEMA.REGISTERSCHEMA(
+        SCHEMAURL => 'http://cc/genres.xsd',
+        SCHEMADOC => BFILENAME('DIRXML', 'genres.xsd'),
+        LOCAL => TRUE, 
+		GENTYPES => TRUE, 
+		GENTABLES => FALSE,
+        CSID => NLS_CHARSET_ID('AL32UTF8')
+    );
+	
+    DBMS_XMLSCHEMA.REGISTERSCHEMA(
+        SCHEMAURL => 'http://cc/film_genre.xsd',
+        SCHEMADOC => BFILENAME('DIRXML', 'film_genre.xsd'),
         LOCAL => TRUE, 
 		GENTYPES => TRUE, 
 		GENTABLES => FALSE,
