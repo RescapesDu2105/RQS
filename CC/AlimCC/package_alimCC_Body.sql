91f9871076317fa75dc0bf0e6deea0ffe052f35b
Doublon <tussetquentin07@hotmail.com>
2017-12-11 11:28:34 +0100

    MaJ AlimCC

--- CC/AlimCC/package_alimCC_Body.sql
+++ CC/AlimCC/package_alimCC_Body.sql
@@ -5,6 +5,7 @@ AS
         l_copy Liste_Copie;
         l_copy_temp Liste_Copie;
         l_id_film Liste_Id_Movie;
+        l_id_arecup Liste_Id_Movie;
         i NUMBER;
         isPresent boolean;
     BEGIN
@@ -35,12 +36,14 @@ AS
             
             /*FOR indx IN l_copy.FIRST .. l_copy.LAST LOOP
                 dbms_output.put_line('copye : '||l_copy(indx).movie);
-            END LOOP;
+            END LOOP;*/
             
-            FOR indx IN l_id_film.FIRST .. l_id_film.LAST LOOP
+            /*FOR indx IN l_id_film.FIRST .. l_id_film.LAST LOOP
                 dbms_output.put_line('film : '||l_id_film(indx));
-            END LOOP;*/          
-            l_id_film:=Check_MovieDescription(l_id_film,1);
+            END LOOP;    */
+            
+            l_id_arecup:=Check_MovieDescription(l_id_film,1);
+            Recup_Data(l_id_arecup);
         END IF;
              
     END Take_Copy;
@@ -58,11 +61,9 @@ AS
             SELECT IdFilm
             FROM FILMS@orcl@cc'||p_complexe||'
             WHERE idFilm='||l_id_film(indx);
+            EXECUTE IMMEDIATE requeteBlock BULK COLLECT INTO l_id_film_temp;
         END LOOP;
         
-
-        EXECUTE IMMEDIATE requeteBlock BULK COLLECT INTO l_id_film_temp;
-        dbms_output.put_line(l_id_film_temp.count);
         IF l_id_film_temp.count>0 THEN
             FOR indx IN l_id_film.FIRST .. l_id_film.LAST LOOP
                 isPresent:=false;
@@ -80,6 +81,52 @@ AS
             RETURN l_id_film;
         END IF;
     END Check_MovieDescription;
+    
+    
+    PROCEDURE Recup_Data(l_id_film IN Liste_Id_Movie) AS
+    BEGIN
+        FOR indx IN l_id_film.FIRST .. l_id_film.LAST LOOP
+            SELECT * INTO l_films(indx)
+            FROM films
+            WHERE idFilm=l_id_film(indx);
+            
+            SELECT Jouer.* INTO l_jouer(indx)
+            FROM Jouer JOIN Films
+            ON Jouer.Film=Films.idFilm
+            Where Films.idFilm=l_id_film(indx);
+            
+            SELECT Realiser.* INTO l_realiser(indx)
+            FROM Realiser JOIN Films
+            ON Realiser.Film=Films.idFilm
+            Where Films.idFilm=l_id_film(indx);
+            
+            SELECT Artists.* INTO l_artists(indx)
+            FROM Artists JOIN Jouer
+            ON Artists.idArt=Jouer.Artist
+            WHERE Jouer.Film=l_id_film(indx);
+            
+            SELECT Certifications.* INTO l_certification(indx)
+            FROM Certifications JOIN Films
+            ON Certifications.idCerti=Films.Certification
+            WHERE Films.idFilm=l_id_film(indx);
+            
+            SELECT Posters.* INTO l_posters(indx)
+            FROM Posters JOIN Films
+            ON Posters.idPoster=Films.Poster
+            WHERE Films.idFilm=l_id_film(indx);
+            
+            SELECT Film_Genre.* INTO l_film_genre(indx)
+            FROM Film_Genre JOIN Films
+            ON Film_Genre.Film=Films.idFilm
+            WHERE Films.idFilm=l_id_film(indx);
+            
+            SELECT Genres.* INTO l_genres(indx)
+            FROM Genres JOIN Film_Genre
+            ON Genres.idGenre=Film_Genre.Genre
+            WHERE Film_Genre.Film=l_id_film(indx);
+        END LOOP;
+        
+    END Recup_Data;
     
 END package_AlimCC;
 --dbms_output.put_line(Liste(indx));
--- CC/AlimCC/package_alimCC_Header.sql
+++ CC/AlimCC/package_alimCC_Header.sql
@@ -3,7 +3,26 @@ IS
     TYPE Liste_Copie IS TABLE OF films_copies%ROWTYPE INDEX BY BINARY_INTEGER;
     TYPE Liste_Id_Movie IS TABLE OF Films.idFilm%TYPE INDEX BY BINARY_INTEGER;
     
+    TYPE Liste_Films IS TABLE OF Films%ROWTYPE INDEX BY BINARY_INTEGER;
+    TYPE Liste_Artists IS TABLE OF Artists%ROWTYPE INDEX BY BINARY_INTEGER;
+    TYPE Liste_Certifications IS TABLE OF Certifications%ROWTYPE INDEX BY BINARY_INTEGER;
+    TYPE Liste_genres IS TABLE OF genres%ROWTYPE INDEX BY BINARY_INTEGER;
+    TYPE Liste_posters IS TABLE OF posters%ROWTYPE INDEX BY BINARY_INTEGER;
+    TYPE Liste_REALISER IS TABLE OF REALISER%ROWTYPE INDEX BY BINARY_INTEGER;
+    TYPE Liste_Film_Genre IS TABLE OF Film_Genre%ROWTYPE INDEX BY BINARY_INTEGER;
+    TYPE Liste_JOUER IS TABLE OF JOUER%ROWTYPE INDEX BY BINARY_INTEGER;
+    TYPE Liste_Films_Copies IS TABLE OF Films_Copies%ROWTYPE INDEX BY BINARY_INTEGER;
+    l_films Liste_Films;
+    l_certification Liste_Certifications;
+    l_artists Liste_Artists;
+    l_genres Liste_Genres;
+    l_posters Liste_Posters;
+    l_realiser Liste_Realiser;
+    l_film_genre Liste_Film_Genre;
+    l_jouer Liste_Jouer;
+    
     PROCEDURE Take_Copy;
     FUNCTION Check_MovieDescription(l_id_film IN Liste_Id_Movie , p_complexe IN NUMBER)
     RETURN Liste_Id_Movie;
+    PROCEDURE Recup_Data(l_id_film IN Liste_Id_Movie);
 END package_AlimCC;
