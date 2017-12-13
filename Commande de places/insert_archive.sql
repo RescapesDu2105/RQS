51c3f776e06a6e2e4cac3cb3fb7832674972ff96
Doublon <tussetquentin07@hotmail.com>
2017-12-13 15:50:08 +0100

    archivage

---
+++ Commande de places/insert_archive.sql
@@ -0,0 +1,13 @@
+INSERT INTO archives
+    VALUES (XMLType(BFILENAME('DIRXML','archivage/archive1.xml'),NLS_CHARSET_ID('AL32UTF8')));
+INSERT INTO archives
+    VALUES (XMLType(BFILENAME('DIRXML','archivage/archive2.xml'),NLS_CHARSET_ID('AL32UTF8')));
+INSERT INTO archives
+    VALUES (XMLType(BFILENAME('DIRXML','archivage/archive3.xml'),NLS_CHARSET_ID('AL32UTF8')));
+INSERT INTO archives
+    VALUES (XMLType(BFILENAME('DIRXML','archivage/archive4.xml'),NLS_CHARSET_ID('AL32UTF8')));
+INSERT INTO archives
+    VALUES (XMLType(BFILENAME('DIRXML','archivage/archive5.xml'),NLS_CHARSET_ID('AL32UTF8')));
+INSERT INTO archives
+    VALUES (XMLType(BFILENAME('DIRXML','archivage/archive6.xml'),NLS_CHARSET_ID('AL32UTF8')));
+commit;
