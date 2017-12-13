f6e84a738a4fb3209a2e2e2474fc01358a964a8d
Doublon <tussetquentin07@hotmail.com>
2017-12-13 11:02:15 +0100

    DeleteCC

---
+++ CC/AlimCC/DeleteCC.sql
@@ -0,0 +1,34 @@
+    req VARCHAR2(20000);
+BEGIN
+    FOR i IN 1 .. 6 LOOP
+    req:='
+        delete from FILMS_COPIES@orcl@cc'||i;
+        EXECUTE IMMEDIATE req;
+    req:='
+        DELETE FROM JOUER@orcl@cc'||i;
+        EXECUTE IMMEDIATE req;
+    req:='
+        DELETE FROM realiser@orcl@cc'||i;
+        EXECUTE IMMEDIATE req;
+    req:='
+        DELETE FROM ARTISTS@orcl@cc'||i;
+        EXECUTE IMMEDIATE req;
+    req:='
+        DELETE FROM Film_Genre@orcl@cc'||i;
+        EXECUTE IMMEDIATE req;
+    req:='
+        DELETE FROM GENRES@orcl@cc'||i;
+        EXECUTE IMMEDIATE req;
+    req:='
+        DELETE FROM films@orcl@cc'||i;
+        EXECUTE IMMEDIATE req;
+    req:='
+        DELETE FROM certifications@orcl@cc'||i;
+        EXECUTE IMMEDIATE req;
+    req:='
+        DELETE FROM posters@orcl@cc'||i;
+        EXECUTE IMMEDIATE req;
+    END loop;
+    commit;
+END ;
+/
