0b52437b49726ffedfecdb4cf356fa85c8ca70fd
Doublon <tussetquentin07@hotmail.com>
2017-12-14 14:12:10 +0100

    copie

--- CC/AlimCC/Insert/cc5.sql
+++ CC/AlimCC/Insert/cc5.sql
@@ -1,3 +1,7 @@
+Insert into CERTIFICATIONS (IDCERTI,NOMCERTI) values ('81','PG');
+
+Insert into POSTERS (IDPOSTER,PATHIMAGE) values ('162','/6u1fYtxG5eqjhtCPDx04pJphQRW.jpg');
+
 INSERT INTO programmations VALUES(28,5,'12/11/2017','12/12/2017',1891,852,1,'14:00');
 INSERT INTO programmations VALUES(29,5,'08/08/2017','08/09/2017',1891,853,2,'17:00');
 commit;
